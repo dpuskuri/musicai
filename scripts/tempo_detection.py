b369f9354bdb12e24ac53983249a8c255a736669
dpuskuri <64773180+dpuskuri@users.noreply.github.com>
2024-06-13 03:32:20 +0200

    Update tempo_detection.py

--- scripts/tempo_detection.py
+++ scripts/tempo_detection.py
@@ -1,14 +1,17 @@
 # tempo_detection.py
 import librosa
 
-# Load the audio file
-audio_file = "audio/CINDERVOMIT.wav"
+def detect_tempo(audio_file):
+    # Load audio data and sampling rate with downsampling
+    y, sr = librosa.load(audio_file, sr=22050, mono=True, res_type='kaiser_fast')
 
-# Load audio data and sampling rate
-y, sr = librosa.load(audio_file)
+    # Calculate tempo
+    tempo, _ = librosa.beat.beat_track(y=y, sr=sr)
 
-# Calculate tempo
-tempo, _ = librosa.beat.beat_track(y=y, sr=sr)
+    return tempo
 
-print(f"Tempo/BPM: {tempo}")
+if __name__ == "__main__":
+    audio_file = "audio/CINDERVOMIT.wav"
+    tempo = detect_tempo(audio_file)
+    print(f"Estimated Tempo/BPM: {tempo}")
 
